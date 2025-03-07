4e6cce9c2e0a14307602459c7463c555ebaccb9e
Ther <1329438302@qq.com>
2025-03-07 10:41:09 +0800

    chore: modify run_gsm8k.sh

--- run_gsm8k.sh
+++ run_gsm8k.sh
@@ -2,17 +2,17 @@
 set -x
 
 #! model parameters
-model_name=llama-3-8b-hf
+model_name=llama-2-7b-hf
 model_dir=/home/yujin-wa20/projects/aliendao
 model_name_full=${model_dir}/${model_name}
 
 #! HyCLoRA core parameters
 use_hyclora=True
-layer_type=baseline
+layer_type=intra
 iteration_threshold=5
 softmax_outlier_ratio=0.05
-layernorm_outlier_ratio=0.005
-q_bit=4
+layernorm_outlier_ratio=0
+q_bit=8
 
 # training parameters
 lora_init_type=qlora
@@ -23,7 +23,7 @@ gradient_accumulation_steps=4
 num_train_epochs=6
 per_device_train_batch_size=4
 evaluation_strategy="no"
-save_strategy="epoch"
+save_strategy="no"
 weight_decay=0.1
 warmup_ratio=0.03
 lr_scheduler_type="cosine"
@@ -33,7 +33,7 @@ do_eval=True
 
 # tag
 tag=${model_name}-${use_hyclora}-${layer_type}-${q_bit}-${layernorm_outlier_ratio}-${softmax_outlier_ratio}
-exp_name=gsm8k-${tag}
+exp_name=gsm8k-${tag}-allquant
 
 mkdir exp_results_gsm8k
 
